a58c0e8ec32ea2bb5d659bf1194a9bdd210a392a
Abdullah osama saber <mr.boode55@gmail.com>
2022-08-11 02:42:37 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,17 @@
+
+<?php
+	$IntNum = $_POST['IntNum'];
+	// Database connection
+	$conn = new mysqli('localhost','root','','SmartMethods');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into SmartMethodsData(Number) values (?)");
+		$stmt->bind_param("i", $IntNum);
+		$execval = $stmt->execute();
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
